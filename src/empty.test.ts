a951a06ab471ac3b1cd85b7318653d75b42e904a
Jesse Kelly <kellyjesse881@gmail.com>
2023-05-25 17:01:20 -0600

    feat: added empty compiler

--- src/empty.test.ts
+++ src/empty.test.ts
@@ -4,7 +4,7 @@ import { pipe } from "@effect/data/Function"
 import * as _ from "./empty"
 
 
-describe("", () => {
+describe("empty", () => {
 
     it("ast", () => {
         const fn = () => 0
@@ -12,5 +12,108 @@ describe("", () => {
         expect(ast).toEqual({
             [_.EmptyHookId]: fn
         })
+    })
+
+    it("set value", () => {
+        const schema = pipe(S.NumberFromString, _.empty(() => 1))
+        const empty = _.emptyFor(schema);
+        expect(empty).toBe(1)
+    })
+
+    it("number", () => {
+        const empty = _.emptyFor(S.number)
+        expect(empty).toBe(0)
+    })
+
+    it("string", () => {
+        const empty = _.emptyFor(S.string);
+        expect(empty).toBe("");
+    });
+
+    it("boolean", () => {
+        const empty = _.emptyFor(S.boolean);
+        expect(empty).toBe(false);
+    });
+
+    it("transform", () => {
+        const schema: S.Schema<string, readonly [string]> = pipe(
+        S.string,
+        S.transform(S.tuple(S.string), (s) => [s] as readonly string[], ([s]) => s))
+        const empty = _.emptyFor(schema)
+
+        expect(empty).toEqual([""])
+    })
+
+    it("tuple", () => {
+        const schema = pipe(S.tuple(S.string, S.number), S.rest(S.boolean))
+        const empty = _.emptyFor(schema)
+        expect(empty).toEqual(["", 0])
+    })
+
+    it("record", () => {
+        const empty = _.emptyFor(S.record(S.string, S.number));
+        expect(empty).toEqual({});
+    });
+
+    it("struct", () => {
+        const schema = pipe(
+          S.struct({
+            a: S.string,
+            b: S.number,
+            c: S.array(S.number),
+            d: S.optional(S.boolean),
+            e: S.struct({
+              f: S.tuple(S.number, S.literal("literal"))
+            }),
+            g: pipe(S.string, _.empty(() => '/'))
+          })
+        )
+        const empty = _.emptyFor(schema)
+    
+        expect(empty).toEqual({ a: "", b: 0, c: [], e: { f: [0, "literal"] }, g: "/" })
+    })
+    
+    it("struct - partial", () => {
+        const schema = S.partial(S.struct({ a: S.string, b: S.number }))
+        const empty = _.emptyFor(schema)
+
+        expect(empty).toEqual({})
+    })
+
+    it("union - discriminated", () => {
+        const schema = S.union(
+        S.struct({ type: S.literal("a"), a: S.string }),
+        S.struct({ type: S.literal("b"), b: S.number })
+        )
+
+        const empty = _.emptyFor(schema)
+        expect(empty).toEqual({ type: "a", a: "" })
+    })
+
+    it("template literal", () => {
+        const schema = S.templateLiteral(S.literal("a"), S.string, S.literal("b"))
+        const empty = _.emptyFor(schema)
+
+        expect(empty).toBe("ab")
+    })
+
+    it("void", () => {
+        const empty = _.emptyFor(S.void)
+        expect(empty).toBeUndefined()
+    })
+
+    it("symbol", () => {
+        const empty = _.emptyFor(S.symbol)
+        expect(empty.toString()).toEqual(Symbol().toString())
+    })
+
+    it("any", () => {
+        const empty = _.emptyFor(S.any)
+        expect(empty).toBeUndefined()
+    })
+
+    it("unknown", () => {
+        const empty = _.emptyFor(S.undefined)
+        expect(empty).toBeUndefined()
     })
 })
--- src/empty.ts
+++ src/empty.ts
@@ -1,9 +1,71 @@
 import * as S from "@effect/schema/Schema"
+import * as O from "@effect/data/Option"
+import * as AST from "@effect/schema/AST"
 import { pipe } from "@effect/data/Function"
 
-export const EmptyHookId = "@effect/schema/annotation/EmptyHookId"
+export const EmptyHookId = "@effect/schema/annotation/EmptyHookId" as const
 
 export const empty = <A>(empty: () => A): <I>(self: S.Schema<I, A>) => S.Schema<I, A> => 
     S.annotations({ [EmptyHookId]: empty })
 
-const ret = pipe(S.NumberFromString, empty(() => 0))
+const getAnnotation = AST.getAnnotation<() => unknown>(
+  EmptyHookId
+)
+
+export const emptyFor = <I, A>(schema: S.Schema<I, A>): A => {
+    const go = (ast: AST.AST): A => {
+
+        const annotations = getAnnotation(ast)
+        if(annotations._tag === "Some") {
+         return annotations.value() as A
+        }
+
+        switch (ast._tag) {
+            case "Literal": return ast.literal as A
+            case "ObjectKeyword": return {} as A
+            case "Tuple": return ast.elements.map((e) => go(e.type)) as A
+            case "BigIntKeyword": return 0n as A
+            case "NumberKeyword": return 0 as A
+            case "StringKeyword": return "" as A
+            case "BooleanKeyword": return false as A
+            case "Refinement": return go(ast.from)
+            case "Transform": return go(ast.to)
+            case "Declaration": return go(ast.type)
+            case "Enums": return ast.enums[0][1] as A
+            case "Union": return go(ast.types[0]) // TODO: Pick the "simplest" value
+            case "TemplateLiteral": {
+                const components = [ast.head]
+                for (const span of ast.spans) {
+                    components.push(span.literal)
+                }
+                return components.join("") as A
+            }
+            case "SymbolKeyword": return Symbol() as A
+            case "UniqueSymbol": return JSON.stringify(ast.symbol.toString()) as A
+            case "TypeLiteral": {
+                const propertySignaturesTypes = ast.propertySignatures.map((f) => go(f.type))
+                const output: any = {}
+
+                for (let i = 0; i < propertySignaturesTypes.length; i++) {
+                    const ps = ast.propertySignatures[i]
+                    const name = ps.name
+                    if (!ps.isOptional) {
+                    output[name] = propertySignaturesTypes[i]
+                    }
+                }
+
+                return output
+            }
+            case "UndefinedKeyword":
+            case "UnknownKeyword":
+            case "VoidKeyword":
+            case "AnyKeyword":
+                return undefined
+        }
+
+        throw new Error(`unhandled ${ast._tag}`)
+
+    }
+
+    return go(schema.ast)
+}
