2f29522ab2aed7ba0811925ed1879a8d199356b9
Jesse Kelly <kellyjesse881@gmail.com>
2023-06-07 18:58:25 -0600

    feat: added enum, literal

--- src/avro.ts
+++ src/avro.ts
@@ -1,12 +1,13 @@
 import * as S from "@effect/schema/Schema"
 import * as AST from "@effect/schema/AST"
-import { Constraints, createHookId } from "./common"
+import { Constraints, combineConstraints, createHookId, getConstraints } from "./common"
+import type { Schema } from "avsc"
 
 
 export const AvroHookId = createHookId("AvroHookId")
 
 interface Avro {
-    (): object
+    (): Schema
 }
 
 export const avro = <I, A, const E extends A = A>(avro: Avro): (self: S.Schema<I, A>) => S.Schema<I, A> => 
@@ -20,8 +21,12 @@ export const to = <I, A>(schema: S.Schema<I, A>): Avro => go(AST.to(schema.ast))
 
 export const from = <I, A>(schema: S.Schema<I, A>): Avro => go(AST.from(schema.ast))
 
+interface Context {
+    optional: boolean
+}
+
 /** @internal */
-const go = (ast: AST.AST, constraints?: Constraints): Avro => {
+const go = (ast: AST.AST, c?: Constraints): Avro => {
 
     const annotations = getAnnotation(ast)
     if(annotations._tag === "Some") {
@@ -30,5 +35,20 @@ const go = (ast: AST.AST, constraints?: Constraints): Avro => {
 
     switch (ast._tag) {
         case "NeverKeyword": throw new Error("cannot convert `never` to AvroSchema")
+
+        case "StringKeyword": return () => ({ "type" : "string" })
+        case "NumberKeyword": return () => {
+            if(c && c._tag === "NumberConstraints" && c.constraints.isInt) {
+                return { "type": "int" }
+            }
+
+            return { "type" : "float" }
+        }
+
+        case "BooleanKeyword": return () => ({ "type": "boolean" })
+        case "Enums": return () => ({ "type": "enum", "symbols": ast.enums.map(e => e[0]), "name": "" })
+        case "Literal": return () => ({ "type": "enum", "symbols": [ast.literal.toString()], "name": "" })
+
+        case "Refinement": return go(ast.from, combineConstraints(c, getConstraints(ast)))
     }
 }
--- tests/avro.test.ts
+++ tests/avro.test.ts
@@ -1,12 +1,48 @@
 import { describe, it, expect } from "vitest";
 import * as S from "@effect/schema/Schema"
-import { pipe } from "@effect/data/Function"
 import * as _ from "../src/avro"
+import type { schema } from "avsc"
+import { pipe } from "@effect/data/Function";
+import { Category, Fruits } from "./common";
+
 
 describe("Avro", () => {
-    it("never", () => {
+    it("never/ ", () => {
         expect(() => _.to(S.never)()).toThrowError(
             new Error("cannot convert `never` to AvroSchema")
         );
+    })
+
+    it("string/ ", () => {
+        const avroSchema = _.to(S.string)();
+        expect(avroSchema).toEqual({ type: "string" } satisfies schema.AvroSchema)
+    })
+
+    it("number/ ", () => {
+        const avroSchema = _.to(S.number)();
+        expect(avroSchema).toEqual({ type: "float" } satisfies schema.AvroSchema)
+    })
+
+    it("number/ int", () => {
+        const schema = pipe(S.number, S.int())
+        const avroSchema = _.to(schema)();
+        expect(avroSchema).toEqual({ type: "int" } satisfies schema.AvroSchema)
+    })
+
+    it("boolean/ ", () => {
+        const avroSchema = _.to(S.boolean)();
+        expect(avroSchema).toEqual({ type: "boolean" } satisfies schema.AvroSchema)
+    })
+
+    it("enum/ ", () => {
+        const schema = S.enums(Fruits)
+        const avroSchema = _.to(schema)();
+        expect(avroSchema).toEqual({ name: "", type: "enum", symbols: ["Apple", "Banana"] } satisfies schema.AvroSchema)
+    })
+
+    it("literal/ ", () => {
+        const val = _.to(S.literal("a"))();
+
+        expect(val).toEqual({ name: "", type: "enum", symbols: ["a"] } satisfies schema.AvroSchema)
     })
 })
